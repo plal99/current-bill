dbcc6a4ecff142b058a5ab241c4a17654969f864
plal <lal.praveen123@gmail.com>
2020-04-30 23:29:33 +0530

    python file 2
    This program uses pyautogui module which has many mouse and keyboard functionalities. The excel file saved in our directory is saved into my google drive account.  As I am logged into my browser, I am by default logged in to google (which means google drive is accessible without any password)

---
+++ drive_automation.py
@@ -0,0 +1,19 @@
+import pyautogui
+from time import sleep
+
+pyautogui.hotkey("winleft","r")
+sleep(0.5)
+pyautogui.typewrite("chrome")
+pyautogui.press("enter")
+sleep(2)
+pyautogui.click(194,51)
+pyautogui.press("backspace")
+pyautogui.typewrite("https://drive.google.com/drive/my-drive", interval = 0.01)          # intervalForBeauty
+pyautogui.press("enter")
+sleep(5)
+pyautogui.click(58,198)                                                                  # addInDrive
+sleep(0.5)
+pyautogui.click(79,256)                                                                  # addFile
+sleep(0.5)
+pyautogui.typewrite(r"C:\.....sujith_readings_new", interval = 0.01)
+pyautogui.press("enter")
