40e2934c30dac621889807e631a52b5c34dcf09d
plal99 <lal.praveen123@gmail.com>
2020-05-01 21:18:34 +0530

    Maximize change made

--- drive_automation.py
+++ drive_automation.py
@@ -6,14 +6,16 @@ sleep(0.5)
 pyautogui.typewrite("chrome")
 pyautogui.press("enter")
 sleep(2)
+pyautogui.hotkey("winleft","up")                                                    # forMaximizingChrome
+sleep(0.5)
 pyautogui.click(194,51)
 pyautogui.press("backspace")
-pyautogui.typewrite("https://drive.google.com/drive/my-drive", interval = 0.01)          # intervalForBeauty
-pyautogui.press("enter") 
+pyautogui.typewrite("https://drive.google.com/drive/my-drive", interval = 0.01)
+pyautogui.press("enter")
 sleep(5)
 pyautogui.click(58,198)                                                                  # addInDrive
 sleep(0.5)
 pyautogui.click(79,256)                                                                  # addFile
 sleep(0.5)
-pyautogui.typewrite(r"C:\.....sujith_readings_new", interval = 0.01)
+pyautogui.typewrite(r"C:\Users\lalpr\Desktop\CURRENT_BILL\sujith_readings_new", interval = 0.01)
 pyautogui.press("enter")
